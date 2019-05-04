cde4c8e2983dd150bdb8a16680736cc7a9737ce4
Lorenzo Solano Martínez <lorenzo.sm@gmail.com>
2019-05-04 15:31:19 -0400

    Initial version.

---
+++ Range.Tests/IntRangeFacts.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Range.Tests
+{
+    [TestFixture]
+    internal sealed class IntRangeFacts
+    {
+        /* R G R: Red Green Refactor
+         * OK 1. Expression is null should fail
+         * OK 2. Expression empty should fail
+         * OK 3. Valid expression should not fail
+         * 4. [2,6) endPoints returns {2, 5}
+         */
+
+        [Test]
+        public void With_Null_Expression_Should_Fail() {
+            string expression = null;
+
+            Assert.That(() => new IntRange(expression),
+                Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void With_Empty_Expression_Should_Fail()
+        {
+            string expression = "";
+
+            Assert.That(() => new IntRange(expression),
+                Throws.ArgumentException);
+        }
+
+        [Test]
+        public void With_Valid_Expression_Should_Pass()
+        {
+            string expression = "[2,6)";
+
+            Assert.That(() => new IntRange(expression),
+                Throws.Nothing);
+        }
+
+        [Test]
+        public void With_Valid_Range_Returns_End_Points()
+        {
+            string expression = "[2,6)";
+
+            IntRange range = new IntRange(expression);
+
+            ISet<int> endPoints = range.GetEndPoints();
+
+            Assert.That(endPoints, Is.EqualTo(new HashSet<int> { 2, 5 }));
+        }
+
+        [Test]
+        public void With_Valid_Range_Returns_End_Points2()
+        {
+            string expression = "[2,6]";
+
+            IntRange range = new IntRange(expression);
+
+            ISet<int> endPoints = range.GetEndPoints();
+
+            Assert.That(endPoints, Is.EqualTo(new HashSet<int> { 2, 6 }));
+        }
+
+    }
+}
---
+++ Range/IntRange.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Range
+{
+    public class IntRange
+    {
+        int min;
+        int max;
+
+        public IntRange(string expression)
+        {
+            if (expression == null)
+                throw new ArgumentNullException();
+
+            if (expression == "")
+                throw new ArgumentException();
+
+            (min, max) = ParseExpression(expression);
+        }
+
+        private (int min, int max) ParseExpression(string expression)
+        {
+            char first = expression[0];
+            char end = expression[expression.Length - 1];
+
+            string rawEnds = expression
+                .Replace("(", string.Empty)
+                .Replace("[", string.Empty)
+                .Replace(")", string.Empty)
+                .Replace("]", string.Empty);
+
+            string[] rawEndsArray = rawEnds.Split(',');
+
+            (int tempMin, int tempMax) = (int.Parse(rawEndsArray[0]), int.Parse(rawEndsArray[1]));
+
+            return (first == '[' ? tempMin : tempMin + 1, end == ']' ? tempMax : tempMax - 1);
+        }
+
+        public ISet<int> GetEndPoints() => new HashSet<int> { min, max };
+    }
+}
